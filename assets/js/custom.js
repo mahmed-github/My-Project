8d3e523f4ad675fcb8b003cc22ee31b8f47e0910
NOMI NOMI <nhr146@outlook.com>
2022-06-29 13:28:57 +0500

    Show Form Result

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -40,11 +40,13 @@ $(document).ready(function () {
             console.log(this.value);
             if(this.value != 'alone'){
                 temp += parseFLOAT(this.value);
-            }
+				$(this).attr('checked', 'checked');
+            } 
         });
 		
 		var value_Status = getResultAccToRange((temp / 10))
-		localStorage.setItem("Result", value_Status);
+		var formClone = $('body').html();
+		localStorage.setItem("Result", formClone);
         $('#final-result').html(value_Status);
 		openPageAccordingly((temp / 10))
     });// End
@@ -61,38 +63,56 @@ $(document).ready(function () {
     function findSum(tis){
         var sum = 0 ;
         $(tis).closest('.questions').find('input:radio').each(function(i,e){
-            var selected_value = $("." + $(e).attr('class').split(' ')[1] + ":checked").val();
-            if($(e).is(":checked")){
-                sum += parseFLOAT(selected_value);
-            }
-        })
-        return sum;
+                var selected_value = $("." + $(e).attr('class').split(' ')[1] + ":checked").val();
+                if($(e).is(":checked")){
+                    sum += parseFLOAT(selected_value);
+                }
+            })
+			
+            return sum;
     }
 	
 	function getResultAccToRange(value){
 		var status = '';
 		var num = parseFLOAT(value);
 		if(num >= 0 && num < 0.5 ){
-			status = " <span style='color:Yellow'>Normal</span>"
+			
+						status = " <span style='color:Yellow'>Normal</span>"
+			
 		}
 		else if(num >= 0.5 && num < 1){
-			status =  " <span style='color:green'>Moderate</span>"
+			
+						status =  " <span style='color:green'>Moderate</span>"
+			
 		}
 		else if (num >= 1){
-			status =  " <span style='color:red'>Severe</span>"	
+			
+						status =  " <span style='color:red'>Severe</span>"
+			
 		}
+		
 		return value + status;
-	}	
+		
+	}
+	
 	function openPageAccordingly(value){
+		
+		
 		var num = parseFLOAT(value);
 		if(num >= 0 && num < 0.5 ){
-			window.open('Normal.html')
+			
+						window.open('Normal.html')
+			
 		}
 		else if(num >= 0.5 && num < 1){
-			window.open('Moderate.html')
+			
+						window.open('Moderate.html')
+			
 		}
 		else if (num >= 1){
-            window.open('Severe.html')
+			
+						window.open('Severe.html')
+			
 		}
 		
 	}
